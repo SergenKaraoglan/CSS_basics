225c4b89ad131e064dc2a296ae0bb5bb2ec23a71
SergenKaraoglan <sergenkaraoglan@outlook.com>
2022-11-27 16:17:27 +0000

    value change oninput, hex value color change on input

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+// color siders
 var red_slider = document.getElementById("red_slider");
 var green_slider = document.getElementById("green_slider");
 var blue_slider = document.getElementById("blue_slider");
+var box_model = document.getElementById("boxModel");
 
 var color_sliders = document.getElementsByClassName("color_slider");
 // assign event listener to each slider to change color of box model
 for (var i = 0; i < color_sliders.length; i++) {
-    color_sliders[i].addEventListener("click", changeColor);
+    color_sliders[i].oninput = changeColor;
 }
 
 // Change color of box model when clicked
 function changeColor(){
-    document.getElementById("boxModel").style.backgroundColor = "rgb(" + red_slider.value + "," 
+    boxModel.style.backgroundColor = "rgb(" + red_slider.value + "," 
         + green_slider.value + "," + blue_slider.value + ")";
     
     // Convert RGB to HEX
     document.getElementById("hexValue").innerHTML = convertRGBToHex();
+    changeTextColor();
 }
 
 // Convert RGB to HEX
@@ -22,4 +25,16 @@ function convertRGBToHex(){
     var hex = "#" + ((1 << 24) + (parseInt(red_slider.value) << 16) + 
         (parseInt(green_slider.value) << 8) + parseInt(blue_slider.value)).toString(16).slice(1);
     return hex
+}
+
+// Change text color based on background color
+function changeTextColor(){
+    var red = parseInt(red_slider.value);
+    var green = parseInt(green_slider.value);
+    var blue = parseInt(blue_slider.value);
+    if (green > 200 && blue > 50 || green > 200 && red > 50 || green > 230) {
+        boxModel.style.color = "black";
+    } else {
+        boxModel.style.color = "white";
+    }
 }
