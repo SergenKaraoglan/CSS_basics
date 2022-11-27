2abfc7d491d7a88a3a5c910db4bebdd31f529fd0
SergenKaraoglan <sergenkaraoglan@outlook.com>
2022-11-27 13:09:28 +0000

    Added hex color value

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+var red_slider = document.getElementById("red_slider");
+var green_slider = document.getElementById("green_slider");
+var blue_slider = document.getElementById("blue_slider");
+
 var color_sliders = document.getElementsByClassName("color_slider");
+// assign event listener to each slider to change color of box model
 for (var i = 0; i < color_sliders.length; i++) {
     color_sliders[i].addEventListener("click", changeColor);
 }
 
 // Change color of box model when clicked
 function changeColor(){
-    document.getElementById("boxModel").style.backgroundColor = "rgb(" + document.getElementById("red_slider").value + "," 
-        + document.getElementById("green_slider").value + "," + document.getElementById("blue_slider").value + ")";
+    document.getElementById("boxModel").style.backgroundColor = "rgb(" + red_slider.value + "," 
+        + green_slider.value + "," + blue_slider.value + ")";
     
-    console.log(document.getElementById("red_slider").value);
     // Convert RGB to HEX
-    //convertRGBToHex();
-
+    document.getElementById("hexValue").innerHTML = convertRGBToHex();
 }
 
 // Convert RGB to HEX
 function convertRGBToHex(){
-    var hex = "#" + ((1 << 24) + (parseInt(document.getElementById("red_slider").value) << 16) + (parseInt(document.getElementById("green_slider").value) << 8) + parseInt(document.getElementById("blue_slider").value)).toString(16).slice(1);
-    document.getElementById("hexValue").innerHTML = hex;
+    var hex = "#" + ((1 << 24) + (parseInt(red_slider.value) << 16) + 
+        (parseInt(green_slider.value) << 8) + parseInt(blue_slider.value)).toString(16).slice(1);
+    return hex
 }
